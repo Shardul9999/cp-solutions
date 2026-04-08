5b27920ef24ea88259e117c61c853ccb1a931262
Shardul9999 <shardulhingane16@gmail.com>
2026-04-08 23:44:05 +0530

    15 ms (78.1%) 48.4 MB (5.98%) 0628-maximum-product-of-three-numbers Easy Array

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int ans1 = nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3];
+        int ans2 = nums[nums.length-1] * nums[0] * nums[1];
+        return Math.max(ans1,ans2);
+    }
+}
