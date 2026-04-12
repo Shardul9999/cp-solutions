56d0c5910914e72702938b9d3ce1deb4e3e2888a
Shardul9999 <shardulhingane16@gmail.com>
2026-04-12 14:56:38 +0530

    2 ms (99.72%) 72.4 MB (74.43%) 0860-lemonade-change Easy Array

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int fives = 0;
+        int tens = 0;
+        int twentys = 0;
+
+        for(int i=0; i<bills.length; i++){
+            if(bills[i] == 5) fives++;
+            else if(bills[i] == 10){
+                if(fives >= 1){
+                    tens++;
+                    fives--;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(bills[i] == 20){
+                if(tens >=1 && fives >= 1){
+                    twentys++;
+                    tens--;
+                    fives--;
+                }
+                else if(fives >= 3){
+                    fives -= 3;
+                    twentys++;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
