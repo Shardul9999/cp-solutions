6d4004ccdfa5681a65f68037bc15247679345384
Shardul9999 <shardulhingane16@gmail.com>
2026-04-17 23:02:34 +0530

    213 ms (42.55%) 76.8 MB (76.43%) 1296-divide-array-in-sets-of-k-consecutive-numbers Medium Array

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) return false;
+
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+        for(int a : hand){
+            mp.put(a, mp.getOrDefault(a, 0) + 1);
+        }
+
+        while(!mp.isEmpty()){
+            int start = mp.firstKey();
+
+            int count = mp.get(start);
+
+            for(int i=0; i<groupSize; i++){
+                int group = start + i;
+
+                if(!mp.containsKey(group) || mp.get(group) < count) return false;
+
+                if(mp.get(group) == count){
+                    mp.remove(group);
+                }
+                else{
+                    mp.put(group, mp.get(group)-count);
+                }
+            }
+        }
+        return true;
+    }
+}
