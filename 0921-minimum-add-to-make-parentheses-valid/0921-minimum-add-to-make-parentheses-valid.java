121f0594a052add5411ba42f3ed1a40ea197d8e6
Shardul9999 <shardulhingane16@gmail.com>
2026-04-22 17:20:37 +0530

    1 ms (66.35%) 42.9 MB (42.05%) 0921-minimum-add-to-make-parentheses-valid Medium String

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int moves = 0;
+        for(int i=0; i<s.length(); i++){
+           if(s.charAt(i) == '('){
+            st.push(s.charAt(i));
+           }
+           else{
+            if(st.empty()){
+                moves++;
+            }
+            else{
+                st.pop();
+            }
+           }
+        }
+
+        while(!st.empty()){
+            moves++;
+            st.pop();
+        }
+
+        return moves;
+    }
+}
