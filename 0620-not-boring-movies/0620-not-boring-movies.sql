c3f8116ff92b74518e71a4c856f3dcc557a3c52c
Shardul9999 <shardulhingane16@gmail.com>
2026-05-17 12:28:44 +0530

    Time: 182 ms (74.87%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT id, movie, description, rating from Cinema
+SELECT * from Cinema
 WHERE id % 2 != 0 AND description != 'boring'
 ORDER By rating DESC;
